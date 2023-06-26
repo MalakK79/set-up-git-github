a4b4bf7ab5d80a7683fb68a6da909367af8dda2a
Malk <malakkhalifa2006@gmail.com>
2023-06-26 12:43:50 -0500

    Initial Commit

---
+++ Trying/TryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryingApp.swift
+//  Trying
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
